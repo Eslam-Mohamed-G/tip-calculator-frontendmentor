99081c1c261f4d4bd73e2d11a4609a089d4bfb61
Eslam Mohamed <islam.gomaa.3rb@gmail.com>
2024-11-23 19:45:09 +0200

    commit for design

--- assist/script.js
+++ assist/script.js
@@ -1,13 +1,23 @@
 // 1. الحصول على جميع الأزرار باستخدام querySelectorAll.
 const buttons = document.querySelectorAll('button');
-const bill = document.getElementById("bill").value
-const person = document.getElementById("person").value
+const bill = document.getElementById("bill")
+const person = document.getElementById("person")
+const amountResult = document.getElementById("amount"); 
+const totalResult = document.getElementById("total"); 
 
 // 2. إنشاء وظيفة مستقلة للتعامل مع حدث النقر.
 const handleClick = (e) => {
   const num = e.target.value;
-  const total = (bill * num) / 100;
-  const amount = total / person
+  const billValue = bill.value
+  const personValue = person.value
+  const total = (billValue * num) / 100;
+  const amount = total / personValue
+  if (e.target.id === 'reset') {
+    clear(); // استدعاء الدالة المخصصة للتفريغ
+    return; // الخروج من الوظيفة
+  }
+  amountResult.textContent = `$${amount}`;
+  totalResult.textContent = `$${total}`;
   console.log(amount);
   console.log(total);
 };
@@ -16,5 +26,12 @@ const handleClick = (e) => {
 buttons.forEach((button) => {
   button.addEventListener('click', handleClick);
 });
+
+function clear(){
+  bill.value = "";
+  person.value = "";
+  amountResult.textContent = "$0.0"
+  totalResult.textContent = "$0.0"
+}
 
 
